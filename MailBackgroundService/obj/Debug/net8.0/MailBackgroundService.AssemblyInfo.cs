adb4e2a42b2b24148ea37b72fedda1a04bcdab05
cvetkozubanov <cvetko.zubanov@servalit.com>
2025-12-23 15:42:00 +0100

    init

--- MailBackgroundService/obj/Debug/net8.0/MailBackgroundService.AssemblyInfo.cs
+++ MailBackgroundService/obj/Debug/net8.0/MailBackgroundService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MailBackgroundService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a43d225f328ce2fde77f74888bde9dfd032cfa02")]
 [assembly: System.Reflection.AssemblyProductAttribute("MailBackgroundService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MailBackgroundService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
