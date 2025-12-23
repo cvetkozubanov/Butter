ee3f16a85d2ede6e32642bc1f39c51f14edbb738
cvetkozubanov <cvetko.zubanov@servalit.com>
2025-12-23 15:16:45 +0100

    init

---
+++ MailBackgroundService/Managers/GoogleTokenManager.cs
@@ -0,0 +1,21 @@
+﻿using Google.Apis.Auth.OAuth2;
+
+namespace MailBackgroundService.Managers
+{
+    public class GoogleTokenManager
+    {
+        public static async Task<string> GetAccessTokenAsync()
+        {
+            // 1. Load the service account credentials from the JSON file
+            GoogleCredential credential = GoogleCredential.FromAccessToken("AIzaSyCzHbyAYRIWyqo2JGJuzhU8B2GjWzGtF60");
+
+            // 2. Define the scope(s) required (e.g., Cloud Storage, BigQuery)
+            string[] scopes = { "www.googleapis.com" };
+            credential = credential.CreateScoped(scopes);
+
+            // 3. Request the access token
+            var token = await credential.UnderlyingCredential.GetAccessTokenForRequestAsync();
+            return token;
+        }
+    }
+}
---
+++ MailBackgroundService/Services/MailBackgroundService.cs
@@ -0,0 +1,65 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using MailBackgroundService.Managers;
+using System.Net;
+
+namespace MailBackgroundService.Services
+{
+    public class MailBackgroundService : BackgroundService
+    {
+        private readonly ILogger<MailBackgroundService> _logger;
+
+        public MailBackgroundService(ILogger<MailBackgroundService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            string[] Scopes = { GmailService.Scope.GmailReadonly };
+            // Load client secrets (credentials.json)
+            try
+            {
+                GoogleCredential credential;
+                using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+                {
+                    // Use the required scopes for your API
+                    credential = GoogleCredential.FromStream(stream)
+                        .CreateScoped(Scopes);
+                }
+
+                var service = new GmailService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Butter",
+                });
+
+                // Keep running until the application stops
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _logger.LogInformation("Service running at: {time}", DateTimeOffset.Now);
+                    //var listRequest = service.Users.Labels.List("me");
+                    //var labels = listRequest.Execute();
+
+                    //if (labels.Labels != null)
+                    //{
+                    //    foreach (var label in labels.Labels)
+                    //    {
+                    //        _logger.LogInformation("Label: {labelName}", label.Name);
+                    //    }
+                    //}
+                    // Wait 5 seconds before the next iteration
+                    await Task.Delay(5000, stoppingToken);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
