2c103f248536a4068c954297acb9768f6d217b9a
cvetkozubanov <cvetko.zubanov@servalit.com>
2025-12-25 17:34:49 +0100

    init

--- MailBackgroundService/Services/UserCredentialsService.cs
+++ MailBackgroundService/Services/UserCredentialsService.cs
@@ -20,69 +20,41 @@ namespace MailBackgroundService.Services
         private UserCredential credential;
         private GoogleAuthorizationCodeFlow flow;
         private readonly ILogger<UserCredentialsService> _logger;
+        private string ClientId = "";
+        private string ClientSecret = "";
         public UserCredentialsService (IConfiguration configuration, ILogger<UserCredentialsService> logger)
         {
             email = configuration["Email"];
             appname = configuration["AppName"];
             redirectURL = configuration["RedirectURL"];
+            ClientId = configuration["ClientId"];
+            ClientSecret = configuration["ClientSecret"];
             _logger = logger;
             if (Directory.Exists(createPath()))
             {
-                //credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                //    new ClientSecrets
-                //    {
-                //        ClientId = "1038892956158-g1pegsbfth319um403517vvdvuu2tid6.apps.googleusercontent.com",
-                //        ClientSecret = "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"
-                //    },
-                //    new[] { GmailService.Scope.GmailReadonly },
-                //    "user", // Unique identifier for the user
-                //    CancellationToken.None,
-                //    new FileDataStore(createPath()) // This saves the token to %appdata%
-                //).Result;
                 flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
                 {
                     ClientSecrets = new ClientSecrets
                     {
-                        ClientId = "1038892956158-g1pegsbfth319um403517vvdvuu2tid6.apps.googleusercontent.com",
-                        ClientSecret = "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"
+                        ClientId = ClientId,
+                        ClientSecret = ClientSecret
                     },
                     Scopes = new[] { GmailService.Scope.GmailReadonly },
-                    // FileDataStore is commonly used
                     DataStore = new FileDataStore(createPath())
                 });
 
-                // Load the credential
                 string userId = "user_id";
                 credential = new UserCredential(flow, "user_id", flow.LoadTokenAsync(userId, CancellationToken.None).Result);
             }
         }
         public AuthorizationCodeRequestUrl setCredentials()
         {
-            //using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
-            //{
-            //    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-            //        GoogleClientSecrets.Load(stream).Secrets,
-            //        [GmailService.Scope.GmailReadonly],
-            //        "user",
-            //        CancellationToken.None,
-            //        new FileDataStore(createPath())).Result;
-            //}
-
-
-            //credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-            //    GoogleClientSecrets.FromFile("credentials.json").Secrets,
-            //    [GmailService.Scope.GmailReadonly],
-            //    "user", // Unique ID for the user
-            //    CancellationToken.None,
-            //    new FileDataStore(createPath()) // Stores token locally
-            //).Result;
-
             flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
             {
                 ClientSecrets = new ClientSecrets
                 {
-                    ClientId = "1038892956158-g1pegsbfth319um403517vvdvuu2tid6.apps.googleusercontent.com",
-                    ClientSecret = "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"
+                    ClientId = ClientId,
+                    ClientSecret = ClientSecret
                 },
                 Scopes = new[] { GmailService.Scope.GmailReadonly },
                 DataStore = new FileDataStore(createPath()), // Persistent store
@@ -91,10 +63,6 @@ namespace MailBackgroundService.Services
             string state = Guid.NewGuid().ToString("N");
             var codeRequest = flow.CreateAuthorizationCodeRequest(redirectURL);
             codeRequest.State = state;
-            //codeRequest.Prompt = "consent"; // Forces consent screen
-            // 1. Redirect user to Google to consent
-            // 2. Capture the code from the redirect_uri
-            // 3. Exchange the code for a token
             return codeRequest;
         }
 
